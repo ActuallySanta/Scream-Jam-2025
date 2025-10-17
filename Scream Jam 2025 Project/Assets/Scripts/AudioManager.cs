ccde719325688262f275553d5020b6308888223f
Santiago Benitez-Fernandez <SantiBenitezFer@outlook.com>
2025-10-16 22:13:24 -0400

    Made the audio manager a singleton

--- Scream Jam 2025 Project/Assets/Scripts/AudioManager.cs
+++ Scream Jam 2025 Project/Assets/Scripts/AudioManager.cs
@@ -7,7 +7,17 @@ public class AudioManager : MonoBehaviour
 
     void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
         DontDestroyOnLoad(gameObject); // Persist across scenes
+
 
     }
 
